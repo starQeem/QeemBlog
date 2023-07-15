599eceaacfeef7a4887b269a13014ef5b3001ca7
Qeem <122252150+starQeem@users.noreply.github.com>
2023-07-15 15:12:25 +0800

    Update constant.java

--- src/main/java/com/starQeem/qeemblog/util/constant.java
+++ src/main/java/com/starQeem/qeemblog/util/constant.java
@@ -12,4 +12,10 @@ public class constant {
     public final static int PAGE_NUM = 1;  //默认页码
     public final static int ZERO = 0;
     public static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+    public static final String BLOG_DETAIL_KEY = "QeemBlog:blogDetail:";
+    public static final String TAG_KEY = "QeemBlog:Tag";
+    public static final String TYPE_KEY = "QeemBlog:Type";
+    public static final int BLOG_DETAIL_TTL = 60*60*24*7;
+    public static final int TAG_TTL = 60*60*24*7;
+    public static final int TYPE_TTL = 60*60*24*7;
 }
